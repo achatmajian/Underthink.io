1286290dbc8ecc442a0d96db6a5e842a72e17280
Jeremy Gill <jeremygill87@gmail.com>
2019-11-02 12:27:23 -0400

    ajax query for TMDB and generate poster image into html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,54 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAFyPMp9Jx66lewKt-mdsNUJsY1DpFbfCQ",
-    authDomain: "underthink-io.firebaseapp.com",
-    databaseURL: "https://underthink-io.firebaseio.com",
-    projectId: "underthink-io",
-    storageBucket: "underthink-io.appspot.com",
-    messagingSenderId: "1068499860720",
-    appId: "1:1068499860720:web:6bb99b9ce3421a635d9635",
-    measurementId: "G-EWEGXJ3FEL"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAFyPMp9Jx66lewKt-mdsNUJsY1DpFbfCQ",
+//     authDomain: "underthink-io.firebaseapp.com",
+//     databaseURL: "https://underthink-io.firebaseio.com",
+//     projectId: "underthink-io",
+//     storageBucket: "underthink-io.appspot.com",
+//     messagingSenderId: "1068499860720",
+//     appId: "1:1068499860720:web:6bb99b9ce3421a635d9635",
+//     measurementId: "G-EWEGXJ3FEL"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
 
-  var database = firebase.database();
+//   var database = firebase.database();
 
 $(document).ready(function(){
 
     console.log("document is ready");
 
 
-    $("#submitButton").on("click", function() {
+    $("#likeButton").on("click", function() {
         
-        $("#displayBox").empty();
+        $("#titlesBox").empty();
 
         callSearch();
     
         //function to initiate search based on query parameters/input
         function callSearch() {
-
-        //on click, submit query
-            var searchInput = $("#searchInput").val().trim();
-        
+         
+        var apiKey = "da2a72f5163ff2c54a74dab6f5cc5bd3";
+        var genreChoice = ["18"];
         //setting for running the query in the API
-            var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=" + searchInput,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-                    "x-rapidapi-key": "ea8d56c7demsh58a2de9c820070ap1858acjsnec3bd46c160d"
-                }
-        }
-        
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + genreChoice[0]+ "&api_key=" + apiKey,
+            "method": "GET",
+            "headers": {},
+            "data": "{}"
+          }
 
-        $.ajax(settings).done(function (response) {
+          
+          $.ajax(settings).done(function (response) {
             console.log(response);
-            var results = response.Search;
-
+         
+          var results = response.results;
             //iterate through all available titles and get path to title image
             for (var i = 0; i < results.length; i++){
-                var picURL = results[i].Poster;
+                var picURL = "https://image.tmdb.org/t/p/w500" + results[i].poster_path;
                 console.log("picURL = " + picURL);
                 var moviePic = $("<img>");
 
@@ -66,8 +63,7 @@ $(document).ready(function(){
 
                 console.log("displaying" + moviePic)
             }
-            
-        });
+             });
 
         
     }
