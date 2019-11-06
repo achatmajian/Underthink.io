74838bca790972b76b79a9fb5521b4ab22ef4ddb
jeremygill87 <54640283+jeremygill87@users.noreply.github.com>
2019-11-06 17:21:37 -0500

    Uservaluespassed (#33)
    * Added basic html for testing and ajaxquery foundation
    
    * ajax query for TMDB and generate poster image into html
    
    * deleted nonsense
    
    * deleted nonsense
    
    * committed for alignment
    
    * fixed up swipe-page IDs
    
    * created another ajax query in imdb to grab metacritic ratings
    
    * added a couple small lines for the metascore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,7 @@ $(".dropdown-trigger").dropdown();
 
       //Functionality in the preliminary genre checkbox screen
       var genreSelections = [];
+      var savedMovies = [];
 
     //read checkboxes and push values into genreSelections array
         $(".checks").on("click", function(){
@@ -39,6 +40,10 @@ $(".dropdown-trigger").dropdown();
         
       //this is just a click function to check the queries.  Will remove later
     $("#likeButton").on("click", function() {
+        $("#posterDisplay").empty();
+        $("#moviePlot").empty();
+        $("#card-title").empty();
+        console.log("clicked");
 
         callSearch();
     
@@ -46,38 +51,81 @@ $(".dropdown-trigger").dropdown();
         
     })
     function callSearch() {
+      console.log("Function called")
          
         var apiKey = "da2a72f5163ff2c54a74dab6f5cc5bd3";
         var randoms = Math.floor(Math.random() * genreSelections.length)
         var randomizer  = Math.floor(Math.random() * 20);
+        
         //setting for running the query in the API
         var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + genreSelections[randoms]+ "&api_key=" + apiKey,
-            "method": "GET",
-            "headers": {},
-            "data": "{}"
-          }
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=" + apiKey,
+          "method": "GET",
+          "headers": {},
+          "data": "{}"
+        }
 
           
           $.ajax(settings).done(function (response) {
-            console.log(response);
+            console.log (response);
+            console.log("Working")
          
           var results = response.results;
                 var picURL = "https://image.tmdb.org/t/p/w500" + results[randomizer].poster_path;
                 var moviePlot = results[randomizer].overview;
-                console.log("picURL = " + picURL);
-                console.log(moviePlot);
                 var moviePic = $("<img>");
+                var movieTitle = results[randomizer].original_title;
+                var movieID = results[randomizer].id;
+                console.log ("Movie ID = " + movieID)
 
                 moviePic.attr("src", picURL);
                 moviePic.attr("alt", "title image");
-                console.log("displaying" + moviePic)
 
-             });
+                $("#posterDisplay").append(moviePic);
+                $("#moviePlot").append(moviePlot);
+                $("#card-title").append(movieTitle);
 
-        
+                var getIMDBsettings = {
+                  "async": true,
+                  "crossDomain": true,
+                  "url": "https://api.themoviedb.org/3/movie/" + movieID + "/external_ids?api_key=" + apiKey,
+                  "method": "GET",
+                  "headers": {},
+                  "data": "{}"
+                }
+                
+                $.ajax(getIMDBsettings).done(function (imdbResponse) {
+                  console.log(imdbResponse);
+
+                  var imdbID = imdbResponse.imdb_id;
+                  console.log (imdbID);
+
+                  var metacriticSettings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://imdb8.p.rapidapi.com/title/get-metacritic?tconst=" + imdbID,
+                    "method": "GET",
+                    "headers": {
+                      "x-rapidapi-host": "imdb8.p.rapidapi.com",
+                      "x-rapidapi-key": "ea8d56c7demsh58a2de9c820070ap1858acjsnec3bd46c160d"
+                    }
+                  }
+                  
+                  $.ajax(metacriticSettings).done(function (metacriticResponse) {
+                    var metascore = metacriticResponse.metaScore;
+                    var reviewCount = metacriticResponse.reviewCount;
+                    var userScore = metacriticResponse.userScore;
+                    var userRatingCount = metacriticResponse.userRatingCount
+                    
+                    console.log(metacriticResponse);
+                    console.log("Metascore: " + metascore + " out of " + reviewCount + " total reviews.");
+                    console.log("User score: " + userScore + " out of " + userRatingCount + " total user reviews.")
+                  });
+                });
+            
+          });
     }
    
 
