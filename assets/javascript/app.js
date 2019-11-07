303aaacd7d61bf77a5522cd2d58a9b6564a89cdf
jeremygill87 <54640283+jeremygill87@users.noreply.github.com>
2019-11-06 20:47:57 -0500

    Ben magic (#42)
    * Added basic html for testing and ajaxquery foundation
    
    * ajax query for TMDB and generate poster image into html
    
    * deleted nonsense
    
    * deleted nonsense
    
    * committed for alignment
    
    * committing
    
    * committing
    
    * pushes movies into saved movies array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,227 +1,239 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(".dropdown-trigger").dropdown();
+	$(".dropdown-trigger").dropdown();
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyAFyPMp9Jx66lewKt-mdsNUJsY1DpFbfCQ",
-        authDomain: "underthink-io.firebaseapp.com",
-        databaseURL: "https://underthink-io.firebaseio.com",
-        projectId: "underthink-io",
-        storageBucket: "underthink-io.appspot.com",
-        messagingSenderId: "1068499860720",
-        appId: "1:1068499860720:web:6bb99b9ce3421a635d9635",
-        measurementId: "G-EWEGXJ3FEL"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      firebase.analytics();
-    
-      var database = firebase.database();
+	var firebaseConfig = {
+		apiKey: "AIzaSyAFyPMp9Jx66lewKt-mdsNUJsY1DpFbfCQ",
+		authDomain: "underthink-io.firebaseapp.com",
+		databaseURL: "https://underthink-io.firebaseio.com",
+		projectId: "underthink-io",
+		storageBucket: "underthink-io.appspot.com",
+		messagingSenderId: "1068499860720",
+		appId: "1:1068499860720:web:6bb99b9ce3421a635d9635",
+		measurementId: "G-EWEGXJ3FEL"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
 
-       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.view').click(function () {
-    $('#modal1').modal('open');
-    $('#modal2').modal('open');
-  });
+	var database = firebase.database();
 
-  $('#modal1').modal();
-  $('#modal2').modal();
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('.view').click(function () {
+		$('#modal1').modal('open');
+		$('#modal2').modal('open');
+	});
 
-
-  var firstName = "";
-  var lastName = "";
-  var userName = "";
-  var email = "";
-  var password = "";
-
-  // On click log in button, checks to make sure user in the in the system
-  // Checks credentials
-  $("#log_in").click(function(){
-    userNameLogin = $("#username-login-input").val().trim();
-    passwordLogin = $("#password-login-input").val().trim();
-
-    console.log(userNameLogin)
-    // console.log(passwordLogin)
-
-    var ref = database.ref("/user-data");
-    ref.once("value").then(function(snapshot){
-      console.log(userNameLogin)
-      var a = snapshot.child(userNameLogin).exists();
-      console.log(a)
-      if(a){
-        firebase.database().ref("/user-data/"+userNameLogin).once("value").then(function(snapshot){
-          var b = snapshot.child("userName").val();
-          var c = snapshot.child("password").val();
-          console.log(b);
-          console.log(c);
-          if(b === userNameLogin && c === passwordLogin){
-            alert("YOUVE SIGNED IN");
-            window.location.href = "swipe-page.html";
-          }
-        })
-      }
-      else if(!a){
-        $("#username-login-input").css("color","red")
-        $("#username-login-label").css("color","red")
-        $("#username-login-label").text("username does not exist")
-      }
-    })  
-  })
+	$('#modal1').modal();
+	$('#modal2').modal();
 
 
-  // On click sign up button, pushes user info to Firebase DB
-  $("#sign_up").click(function () {
+	var firstName = "";
+	var lastName = "";
+	var userName = "";
+	var email = "";
+	var password = "";
+	var globalResult = "";
 
-    event.preventDefault();
+	// On click log in button, checks to make sure user in the in the system
+	// Checks credentials
+	$("#log_in").click(function () {
+		userNameLogin = $("#username-login-input").val().trim();
+		passwordLogin = $("#password-login-input").val().trim();
 
-    // Grabbed values from text-boxes
-    firstName = $("#first_name-input").val().trim();
-    lastName = $("#last_name-input").val().trim();
-    userName = $("#username-input").val().trim();
-    email = $("#email-input").val().trim();
-    password = $("#password-input").val().trim();
+		console.log(userNameLogin)
+		// console.log(passwordLogin)
 
-    // Does not allow for sign up if the username is alredy in use
-    database.ref("/user-data/").once("value", snapshot => {
-      if (snapshot.child(userName).exists()) {
-        console.log("exists!");
-        console.log(userName)
-        $("#username-input").css("color","red")
-        $("#username-label").css("color","red")
-        $("#username-label").text("username already in use")
-      }
-      else {
-        // Code for "Setting values in the database"
-        database.ref("/user-data/" + userName).set({
-          firstName: firstName,
-          lastName: lastName,
-          userName: userName,
-          email: email,
-          password: password,
-        });
+		var ref = database.ref("/user-data");
+		ref.once("value").then(function (snapshot) {
+			console.log(userNameLogin)
+			var a = snapshot.child(userNameLogin).exists();
+			console.log(a)
+			if (a) {
+				firebase.database().ref("/user-data/" + userNameLogin).once("value").then(function (snapshot) {
+					var b = snapshot.child("userName").val();
+					var c = snapshot.child("password").val();
+					console.log(b);
+					console.log(c);
+					if (b === userNameLogin && c === passwordLogin) {
+						alert("YOUVE SIGNED IN");
+						window.location.href = "swipe-page.html";
+					}
+				})
+			}
+			else if (!a) {
+				$("#username-login-input").css("color", "red")
+				$("#username-login-label").css("color", "red")
+				$("#username-login-label").text("username does not exist")
+			}
+		})
+	})
 
-        $("#modal1").modal("close");
-        window.location.href = "checkbox.html"
 
-      }
+	// On click sign up button, pushes user info to Firebase DB
+	$("#sign_up").click(function () {
 
-    });
+		event.preventDefault();
 
-  });
+		// Grabbed values from text-boxes
+		firstName = $("#first_name-input").val().trim();
+		lastName = $("#last_name-input").val().trim();
+		userName = $("#username-input").val().trim();
+		email = $("#email-input").val().trim();
+		password = $("#password-input").val().trim();
 
-      //Functionality in the preliminary genre checkbox screen
-      var genreSelections = [];
-      var savedMovies = [];
+		// Does not allow for sign up if the username is alredy in use
+		database.ref("/user-data/").once("value", snapshot => {
+			if (snapshot.child(userName).exists()) {
+				console.log("exists!");
+				console.log(userName)
+				$("#username-input").css("color", "red")
+				$("#username-label").css("color", "red")
+				$("#username-label").text("username already in use")
+			}
+			else {
+				// Code for "Setting values in the database"
+				database.ref("/user-data/" + userName).set({
+					firstName: firstName,
+					lastName: lastName,
+					userName: userName,
+					email: email,
+					password: password,
+				});
 
-    //read checkboxes and push values into genreSelections array
-        $(".checks").on("click", function(){
-          var x = $(this).attr("value");
-          genreSelections.push(x);
-          console.log(genreSelections)
-        })
+				$("#modal1").modal("close");
+				window.location.href = "checkbox.html"
 
-        //PASS THE GENRESELECTIONS ARRAY INTO THE USER OBJECT IN FIREBASE
-        $("#formSubmit").on("click", function(){
-          database.ref("/user-data/" + userName + "/genreselections").set({
-            genreSelections: genreSelections
-          })
-        })
-        
-        callSearch();
-        
-      //this is just a click function to check the queries.  Will remove later
-    $("#likeButton").on("click", function() {
-        $("#posterDisplay").empty();
-        $("#moviePlot").empty();
-        $("#card-title").empty();
-        console.log("clicked");
+			}
 
-        callSearch();
-    
-        //function to initiate search based on query parameters/input
-        
-    })
-    function callSearch() {
-      console.log("Function called")
-         
-        var apiKey = "da2a72f5163ff2c54a74dab6f5cc5bd3";
-        var randoms = Math.floor(Math.random() * genreSelections.length)
-        var randomizer  = Math.floor(Math.random() * 20);
-        
-        //setting for running the query in the API
-        var settings = {
-          "async": true,
-          "crossDomain": true,
-          "url": "https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=" + apiKey,
-          "method": "GET",
-          "headers": {},
-          "data": "{}"
-        }
+		});
 
-          
-          $.ajax(settings).done(function (response) {
-            console.log (response);
-            console.log("Working")
-         
-          var results = response.results;
-                var picURL = "https://image.tmdb.org/t/p/w500" + results[randomizer].poster_path;
-                var moviePlot = results[randomizer].overview;
-                var moviePic = $("<img>");
-                var movieTitle = results[randomizer].original_title;
-                var movieID = results[randomizer].id;
-                console.log ("Movie ID = " + movieID)
+	});
 
-                moviePic.attr("src", picURL);
-                moviePic.attr("alt", "title image");
+	//Functionality in the preliminary genre checkbox screen
+	var genreSelections = [];
+	var savedMovies = [];
 
-                $("#posterDisplay").append(moviePic);
-                $("#moviePlot").append(moviePlot);
-                $("#card-title").append(movieTitle);
+	//read checkboxes and push values into genreSelections array
+	$(".checks").on("click", function () {
+		var x = $(this).attr("value");
+		genreSelections.push(x);
+	})
 
-                var getIMDBsettings = {
-                  "async": true,
-                  "crossDomain": true,
-                  "url": "https://api.themoviedb.org/3/movie/" + movieID + "/external_ids?api_key=" + apiKey,
-                  "method": "GET",
-                  "headers": {},
-                  "data": "{}"
-                }
-                
-                $.ajax(getIMDBsettings).done(function (imdbResponse) {
-                  console.log(imdbResponse);
+	//PASS THE GENRESELECTIONS ARRAY INTO THE USER OBJECT IN FIREBASE
+	$("#formSubmit").on("click", function () {
+		database.ref("/user-data/" + userName + "/genreselections").set({
+			genreSelections: genreSelections
+		})
+	})
 
-                  var imdbID = imdbResponse.imdb_id;
-                  console.log (imdbID);
+	callSearch();
 
-                  var metacriticSettings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://imdb8.p.rapidapi.com/title/get-metacritic?tconst=" + imdbID,
-                    "method": "GET",
-                    "headers": {
-                      "x-rapidapi-host": "imdb8.p.rapidapi.com",
-                      "x-rapidapi-key": "ea8d56c7demsh58a2de9c820070ap1858acjsnec3bd46c160d"
-                    }
-                  }
-                  
-                  $.ajax(metacriticSettings).done(function (metacriticResponse) {
-                    var metascore = metacriticResponse.metaScore;
-                    var reviewCount = metacriticResponse.reviewCount;
-                    var userScore = metacriticResponse.userScore;
-                    var userRatingCount = metacriticResponse.userRatingCount
-                    
-                    console.log(metacriticResponse);
-                    console.log("Metascore: " + metascore + " out of " + reviewCount + " total reviews.");
-                    console.log("User score: " + userScore + " out of " + userRatingCount + " total user reviews.")
-                  });
-                });
-            
-          });
-    }
-   
 
- 
-  console.log(userName)
+
+	//function to initiate search based on query parameters/input
+	function callSearch() {
+		$("#posterDisplay").empty();
+		$("#moviePlot").empty();
+		$("#card-title").empty();
+
+		console.log("Function called")
+
+		var apiKey = "da2a72f5163ff2c54a74dab6f5cc5bd3";
+		var randoms = Math.floor(Math.random() * genreSelections.length)
+		var randomizer = Math.floor(Math.random() * 20);
+
+		//setting for running the query in the API
+		var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=" + apiKey,
+			"method": "GET",
+			"headers": {},
+			"data": "{}"
+		}
+
+
+		$.ajax(settings).done(function (response) {
+			
+			var results = response.results;
+			
+			var picURL = "https://image.tmdb.org/t/p/w500" + results[randomizer].poster_path;
+			var moviePlot = results[randomizer].overview;
+			var moviePic = $("<img>");
+			var movieTitle = results[randomizer].original_title;
+			var movieID = results[randomizer].id;
+			console.log(results[randomizer])
+			moviePic.attr("src", picURL);
+			moviePic.attr("alt", "title image");
+
+			setResults(results[randomizer]);
+
+			$("#posterDisplay").append(moviePic);
+			$("#moviePlot").append(moviePlot);
+			$("#card-title").append(movieTitle);
+
+			var getIMDBsettings = {
+				"async": true,
+				"crossDomain": true,
+				"url": "https://api.themoviedb.org/3/movie/" + movieID + "/external_ids?api_key=" + apiKey,
+				"method": "GET",
+				"headers": {},
+				"data": "{}"
+			}
+
+			$.ajax(getIMDBsettings).done(function (imdbResponse) {
+
+				var imdbID = imdbResponse.imdb_id;
+
+				var metacriticSettings = {
+					"async": true,
+					"crossDomain": true,
+					"url": "https://imdb8.p.rapidapi.com/title/get-metacritic?tconst=" + imdbID,
+					"method": "GET",
+					"headers": {
+						"x-rapidapi-host": "imdb8.p.rapidapi.com",
+						"x-rapidapi-key": "ea8d56c7demsh58a2de9c820070ap1858acjsnec3bd46c160d"
+					}
+				}
+
+				$.ajax(metacriticSettings).done(function (metacriticResponse) {
+					var metascore = metacriticResponse.metaScore;
+					var reviewCount = metacriticResponse.reviewCount;
+					var userScore = metacriticResponse.userScore;
+					var userRatingCount = metacriticResponse.userRatingCount
+
+				});
+
+			});
+
+			
+
+
+		});
+
+	}
+		//Like a movie and save it for later
+			$("#likeButton").on("click", function () {
+
+				saveMovie(getResult());
+				console.log(savedMovies);
+
+				callSearch();
+			})
+
+	function saveMovie(movie){
+		savedMovies.push(movie);
+	}
+	function setResults(results) {
+		globalResult = results;
+	}
+
+	function getResult(){
+		return globalResult;
+	}
+
+	console.log(userName)
 })
 
 
